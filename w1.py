d697803c9b22b68ad45d2879b1270ccee5c8f846
Mihir Katyal <mkatyal@uci.edu>
2024-01-29 22:03:54 -0800

    if name main added and various corrections made

--- w1.py
+++ w1.py
@@ -18,3 +18,11 @@ def process_file(input_filename, output_filename):
         print(f"Error: File '{input_filename}' not found.")
     except Exception as e:
         print(f"An error occurred: {e}")
+
+if __name__ == '__main__':
+    if len(sys.argv) != 3:
+        print("Usage: python wp1.py input_filename output_filename")
+    else:
+        input_filename = sys.argv[1]
+        output_filename = sys.argv[2]
+        process_file(input_filename, output_filename)

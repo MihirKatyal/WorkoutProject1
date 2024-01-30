7638ee94055ff98d5ff51aa04cbe7937975b13c6
Mihir Katyal <mkatyal@uci.edu>
2024-01-29 22:00:58 -0800

    def process_file created

--- w1.py
+++ w1.py
@@ -2,3 +2,13 @@ import sys
 
 def reverse_cipher(text):
     return text[::-1]
+
+def process_file(input_filename, output_filename):
+    try:
+        with open(input_filename, 'r', encoding='utf-8') as input_file:
+            content = input_file.read()
+
+        reversed_content = reverse_cipher(content)
+
+        with open(output_filename, 'w', encoding='utf-8') as output_file:
+            output_file.write(reversed_content)

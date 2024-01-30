d31ba4c52bdd6db869bef08920fc8d6b3a4f018e
Mihir Katyal <mkatyal@uci.edu>
2024-01-29 22:01:48 -0800

    added print statements

--- w1.py
+++ w1.py
@@ -12,3 +12,9 @@ def process_file(input_filename, output_filename):
 
         with open(output_filename, 'w', encoding='utf-8') as output_file:
             output_file.write(reversed_content)
+
+            print(f"Successfully encrypted '{input_filename}' and saved the result to '{output_filename}'.")
+    except FileNotFoundError:
+        print(f"Error: File '{input_filename}' not found.")
+    except Exception as e:
+        print(f"An error occurred: {e}")
